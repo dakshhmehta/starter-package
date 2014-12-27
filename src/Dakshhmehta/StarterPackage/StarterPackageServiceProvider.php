9f076b131ca60d080034062c83269d2c9c7ef753
Daksh Mehta <daksh@dristal.com>
2014-12-27 19:50:02 +0530

    Intitalized

---
+++ src/Dakshhmehta/StarterPackage/StarterPackageServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Dakshhmehta\StarterPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class StarterPackageServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('dakshhmehta/starter-package');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
